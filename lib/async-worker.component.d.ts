669e6ac447f774bea030c023e9ae7451b60ab745
felipemarts <felipebrasil93@gmail.com>
2022-02-24 00:29:03 -0300

    version 1.0.2

--- lib/async-worker.component.d.ts
+++ lib/async-worker.component.d.ts
@@ -1,4 +1,5 @@
 import { Component, LifeCycleObserver } from '@loopback/core';
 export declare class AsyncWorkerComponent implements Component, LifeCycleObserver {
+    static MAIN_FILE: string;
     start(): Promise<void>;
 }
